3a39ff19bafebdc441b047f209b377070b02d320
julietteseive <julietteseive@gmail.com>
2015-01-14 14:20:56 -0500

    finished rough front-end

--- flask/app/views.py
+++ flask/app/views.py
@@ -14,11 +14,22 @@ def index():
     <subtitle> Let's Disentangle Narratives!</subtitle>
   </head>
   <body>
-    <h3>Please enter your text here, or upload it.</h3>
+    <h3>Please enter your text here or choose to upload it.</h3>
 What book are we looking at today?
 <form action="http://www.google.com">
     <textarea cols="50" rows="5">
     </textarea>
+<br>
+<input type="submit"/>
+</form>
+</br>
+<br>
+<form name="myWebForm" action="mailto:julietteseive@gmail.com" method="post">
+<input type="file" name="uploadField" />
+<input type="submit" value="Submit" />
+</form>
+</br>
+
   </body>
 </html>
 '''
